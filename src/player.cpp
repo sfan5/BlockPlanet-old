ab1362e02c7c61e875c2817be792afd851a10694
Joel Leclerc <mijyn@mail.com>
2012-05-12 20:24:58 -0600

    Changed the camera thing a bit (sorta fixed issue #9)

--- src/player.cpp
+++ src/player.cpp
@@ -46,7 +46,8 @@ Player::Player(IGameDef *gamedef):
 	hp(PLAYER_MAX_HP),
 	hurt_tilt_timer(0),
 	hurt_tilt_timer_max(0),
-	sprinting_timer(0.0),
+	// For now this hack will work
+	sprinting_timer(2.9999),
 	enable_sprinting_timer(0),
 	enable_flying_timer(0),
 	hunger(PLAYER_MAX_HUNGER),
